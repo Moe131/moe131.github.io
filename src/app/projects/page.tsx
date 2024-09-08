b4106b22f745e1284cefe0053718c172f358a06b
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-09-08 01:51:36 -0700

    project added

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,6 +7,7 @@ import foodDeliveryLogo from "../../assets/food-delivery.png"
 import cryptofolioLogo from "../../assets/cryptofolio-logo.png"
 import playlistDownloaderLogo from "../../assets/playlistDownloader-logo.png"
 import ProjectCard from "@/components/ProjectCard/page";
+import chatAppLogo from "../../assets/liveChatApp.png"
 
 
 
@@ -25,6 +26,7 @@ export default function Projects(){
             description = "A React Native full stack application designed to help users to sign up, find nearby restaurants and place food delivery or pick-up orders."
             />
 
+
             <ProjectCard 
             image={cryptofolioLogo} 
             title="CryptoFolio Web Application"
@@ -34,10 +36,19 @@ export default function Projects(){
             description = "A ReactJS full-stack web application that allows users to sign up, track the current prices of various cryptocurrencies, and create and edit custom watchlists."
             />
 
+            <ProjectCard
+            image={chatAppLogo} 
+            title="Next.js Real-time Chat Application"
+            technologies={["Next.js", "TypeScript", "NextAuth","Redis", "Pusher", "WebSocket", "TailwindCSS"]}
+            githubLink="https://github.com/Moe131/realtime_chat_app"  
+            demoLink="https://youtu.be/MrMaQgtBnpE?si=oYHOOoufxOskiMuv"  
+            description="A real-time chat application built with Next.js and TypeScript. Users can log in using Google authentication, add friends and chat with them in real-time"
+            />
+
             <ProjectCard 
             image={searchEngineLogo} 
             title="Search Engine Web Application"
-            technologies= {["python", "Flask", "OpenAI API"]}
+            technologies= {["Python", "Flask", "OpenAI API"]}
             githubLink= "https://github.com/Moe131/Search_engine_web_app"
             demoLink= "https://github.com/Moe131/Search_engine_web_app"
             description = "Search engine web application developed with Python and Flask that allows searching over hundreds of thousand of websites."
@@ -46,7 +57,7 @@ export default function Projects(){
             <ProjectCard 
             image={hotelReservationLogo} 
             title="Hotel Reservation System"
-            technologies= {["python", "Tkinter", "SQLite"]}
+            technologies= {["Python", "Tkinter", "SQLite"]}
             githubLink= "https://github.com/Moe131/Hotel_Reservation_System"
             demoLink= "https://github.com/Moe131/Hotel_Reservation_System"
             description = "Simple hotel reservation system built with Python and Tkinter to store, manage and delete bookings using SQLite database."
@@ -55,7 +66,7 @@ export default function Projects(){
             <ProjectCard 
             image={crawlerLogo} 
             title="Web Crawler"
-            technologies= {["python", "BeautifulSoup", "Git", "Simhash"]}
+            technologies= {["Python", "BeautifulSoup", "Git", "Simhash"]}
             githubLink ="https://github.com/Moe131/webcrawler"
             demoLink= "https://github.com/Moe131/webcrawler"
             description = "A Python-based web crawler designed to efficiently scrape, parse, and store data from websites avoiding loops and duplicate websites "
@@ -64,7 +75,7 @@ export default function Projects(){
             <ProjectCard 
             image={playlistDownloaderLogo} 
             title="Spotify Playlist Song Downloader"
-            technologies= {["python", "Selenium", "PyTube", "REST API"]}
+            technologies= {["Python", "Selenium", "PyTube", "REST API"]}
             githubLink= "https://github.com/Moe131/playlist-downloader"
             demoLink= "https://github.com/Moe131/playlist-downloader"
             description = "This application allows you to download songs from a Spotify playlist by searching and downloading them from YouTube using Selenium and Pytube library."
