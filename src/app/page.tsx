5ac7435c84f4ea3aa27d92aa8e85b530bcf9e5cd
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-09-27 21:58:03 -0700

    experience box added to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Hero from "@/components/Hero/page";
 import Navbar from "@/components/Navbar/page";
-import { usePathname } from "next/navigation";
+import uciLogo from "../assets/uci.png"
+import ivcLogo from "../assets/ivc.png"
+import Image from "next/image";
+import ExperienceBox from "@/components/ExperienceBox/ExperienceBox";
 
 export default function Home() {
 
   return (
-    <main className="lg:max-h-screen bg-gray-100 border-gray-200 dark:bg-gray-900">
+    <main className="min-h-screen bg-gray-100 border-gray-200 dark:bg-gray-900 pb-4">
       <Hero />
+      <ExperienceBox />
     </main>
   );
 }
